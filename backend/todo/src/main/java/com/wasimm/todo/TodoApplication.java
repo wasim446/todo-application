e6943c39dd5b181ce2228200b8f75346a678b8c6
Wasim Ahmad <wasimnmd15@gmail.com>
2026-04-26 21:00:55 +0530

    Security changed.

--- backend/todo/src/main/java/com/wasimm/todo/TodoApplication.java
+++ backend/todo/src/main/java/com/wasimm/todo/TodoApplication.java
@@ -1,9 +1,13 @@
 package com.wasimm.todo;
 
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+import java.util.Base64;
 
 @SpringBootApplication
 @EnableJpaAuditing
@@ -12,6 +16,12 @@ public class TodoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(TodoApplication.class, args);
+        System.out.println("ToDo Full stack application is running...");
+
+//        var key = Keys.secretKeyFor(SignatureAlgorithm.HS256);
+//        String base64 = Base64.getEncoder().encodeToString(key.getEncoded());
+//        System.out.println(base64);
+
 	}
 
 }
--- backend/todo/src/main/java/com/wasimm/todo/security/JwtService.java
+++ backend/todo/src/main/java/com/wasimm/todo/security/JwtService.java
@@ -1,21 +1,41 @@
 package com.wasimm.todo.security;
 
-
 import com.wasimm.todo.Entity.User;
+import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
+import jakarta.annotation.PostConstruct;
 import org.springframework.stereotype.Service;
-
+import java.util.Base64;
 import java.util.Date;
 import org.springframework.beans.factory.annotation.Value;
+
+import javax.crypto.SecretKey;
+
 @Service
 public class JwtService {
 
-  //  private final String SECRET_KEY = "your-very-secret-key-change-this";
+    //  private final String SECRET_KEY = "your-very-secret-key-change-this";
 
     @Value("${jwt.secret}")
     private String SECRET_KEY;
+
+//    private SecretKey SECRET_KEY;
+//
+    private SecretKey getSigningKey() {
+        byte[] keyBytes = Decoders.BASE64.decode(SECRET_KEY);
+        return Keys.hmacShaKeyFor(keyBytes);
+    }
+
+    @PostConstruct
+    public void init() {
+        System.out.println("SECRET VALUE = ");
+
+//        System.out.println("JWT Key length: " + SECRET_KEY.getEncoded().length);
+
+    }
 
     public String generateToken(User user) {
 
@@ -26,20 +46,31 @@ public class JwtService {
                 .claim("userId", user.getId())
                 .setIssuedAt(new Date())
                 .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 60)) // 1 hour
-                .signWith(Keys.hmacShaKeyFor(SECRET_KEY.getBytes()), SignatureAlgorithm.HS256)
+                .signWith(getSigningKey(), SignatureAlgorithm.HS256)
                 .compact();
     }
 
+    // Extract Username
     public String extractUsername(String token) {
-        return Jwts.parserBuilder()
-                .setSigningKey(SECRET_KEY.getBytes())
-                .build()
-                .parseClaimsJws(token)
-                .getBody()
-                .getSubject();
+        return getClaims(token).getSubject();
     }
 
+    // Validate Token
     public boolean isTokenValid(String token, String username) {
-        return extractUsername(token).equals(username);
+        return extractUsername(token).equals(username) && !isTokenExpired(token);
+    }
+
+    // Check Expiry
+    public boolean isTokenExpired(String token) {
+        return getClaims(token).getExpiration().before(new Date());
+    }
+
+    // Common method to parse claims
+    private Claims getClaims(String token) {
+        return Jwts.parserBuilder()
+                .setSigningKey(getSigningKey()) // FIXED.
+                .build()
+                .parseClaimsJws(token)
+                .getBody();
     }
 }
